ec4b25921e45e05f5e12e54bce79f40b3d95fb02
Bob Tierney <bob.tierney@verizon.net>
2018-08-28 07:16:22 -0500

    first commit

---
+++ home/home.js
@@ -0,0 +1,100 @@
+'use strict';
+
+angular.module('myApp.home', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.controller('homeCtrl', ['$scope', '$uibModal', 'masterSvc', function( $scope, $uibModal, masterSvc) {
+  console.log ('in home ctrl');
+  
+  var promise = masterSvc.getTenantsRemote();
+  promise.then (
+    function(payload){
+      $scope.tenants = payload.data;
+      masterSvc.setTenantsLocal (payload.data);
+      console.log (payload.data);
+    }
+  )
+
+  $scope.openRegister = function (){
+    var parentElem = undefined; 
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'templates/rhsso_register.html',
+      controller: 'registerCtrl',
+      controllerAs: '$scope',
+      // size: 'lg',
+      appendTo: parentElem,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+  }
+
+  $scope.openDetails = function (tenant) {
+    console.log ('TENANT:' + tenant);
+    masterSvc.setCurrentTenant (tenant);
+    var parentElem = undefined; 
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'templates/tabview.html',
+      controller: 'apiDetailsCtrl',
+      controllerAs: '$scope',
+      size: 'lg',
+      appendTo: parentElem,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      console.info('Modal dismissed at: ' + new Date());
+    });
+
+};
+
+}])
+
+.controller('registerCtrl', function ($scope, $uibModalInstance){
+  $scope.ok = function () {
+    $uibModalInstance.close();
+  };
+})
+
+.controller('apiDetailsCtrl', function ($scope, $uibModalInstance, masterSvc, tenantSvc) {
+    var currTenant = masterSvc.getCurrentTenant();
+    $scope.mappingList = [];
+    $scope.tenantDetail = {};
+    $scope.tenantDetail = masterSvc.getTenantDetail(currTenant);
+    $scope.ok = function () {
+      $uibModalInstance.close();
+    };
+
+    $scope.changeTab = function (api) {
+      var tid = $scope.tenantDetail.id;
+      var sid = api.service_id;
+      var promise = tenantSvc.getMappingList (tid, sid);
+      promise.then (
+        function(payload){
+          // mapping_rules [{id: 16, metric_id: 21, pattern: "/", http_method: "GET", delta: 1, …}]
+          $scope.mappingList = payload.data.mapping_rules;
+          console.log (payload.data.mapping_rules[0]);
+        }
+      )      
+    }
+  });
---
+++ services.js
@@ -0,0 +1,77 @@
+angular.module('myApp.services', ['ui.bootstrap'])
+.factory('modalFactory', function($uibModal) {
+    return {
+      open: function(size, template, params) {
+        return $uibModal.open({
+          animation: true,
+          templateUrl: template || 'myModalContent.html',
+          controller: 'ModalResultInstanceCtrl',
+          size: size,
+          resolve: {
+            params: function() {
+              return params;
+            }
+          }
+        });
+      }
+    };
+  })
+
+.service ('tenantSvc', function ($http){
+    var getMappingList = function (tid, sid){
+        // http://localhost:8001/threescale/tenants/10/mappinglist/10
+        baseurl = 'http://localhost:8001/threescale/tenants/' + tid + '/mappinglist/' + sid;
+        return $http.get(baseurl);
+    }
+
+    return {
+        getMappingList : getMappingList
+    }
+})
+
+.service ('masterSvc', function ($http){
+    var baseurl = 'http://localhost:8001/threescale/tenants';
+    var currTenant = "";
+    var localTenants = [];
+
+    var getTenantsRemote = function (){
+        return $http.get(baseurl);
+    }
+
+    var getTenantsLocal = function (){
+        return localTenants;
+    }
+
+    var setTenantsLocal = function (tenants){
+        localTenants = tenants;
+    }
+
+    var setCurrentTenant = function (tenant){
+        currTenant = tenant;
+    }
+
+    var getCurrentTenant = function (){
+        return currTenant;
+    }
+
+    var getTenantDetail = function (tenant){
+        var detail = {};
+
+        localTenants.forEach ( function (item, index, array){
+            if (item.name === tenant){
+                console.log (item)
+                detail = item;
+            }
+        });
+        return detail;
+    }
+
+    return {
+        getTenantsRemote : getTenantsRemote,
+        getCurrentTenant : getCurrentTenant,
+        setCurrentTenant : setCurrentTenant,
+        setTenantsLocal : setTenantsLocal,
+        getTenantsLocal : getTenantsLocal,
+        getTenantDetail : getTenantDetail
+    }
+})
